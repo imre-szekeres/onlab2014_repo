622bf5fcdc61e52f190011e8f91e543e82c69a48
onlab2014 <icloudred@gmail.com>
2014-10-02 13:29:43 +0200

    WorkflowManager[secure-spring]: secure Logging hozzaadasa.. log4j.properties -ben a rootLogger inicializalasa, maganak a log4j -nek az inicializalasa (PropertyConfigurator -n keresztul, hogy egyseges legyen a log output a console -n [Spring is log -ol], habar igy FileNotFoundException jon startup-time -kor, ellenben az altalunk megadott config alapjan allitja ossze a log uzeneteket.

--- WorkflowManager/src/main/java/hu/bme/aut/wman/listeners/WebAppStartupListener.java
+++ WorkflowManager/src/main/java/hu/bme/aut/wman/listeners/WebAppStartupListener.java
@@ -3,6 +3,10 @@
  */
 package hu.bme.aut.wman.listeners;
 
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 import hu.bme.aut.wman.model.Role;
 import hu.bme.aut.wman.model.User;
 import hu.bme.aut.wman.service.PrivilegeService;
@@ -19,6 +23,10 @@ import org.springframework.context.event.ContextRefreshedEvent;
  */
 public class WebAppStartupListener 
 					implements ApplicationListener<ContextRefreshedEvent>{
+	
+	private static final Logger LOGGER = Logger.getLogger(WebAppStartupListener.class);
+	public static final String LOGFILE_LOCATION = "WEB-INF/classes/log4j.properties";
+	
 
 	@EJB(mappedName="java:module/UserService")
 	private UserService userService;
@@ -31,19 +39,26 @@ public class WebAppStartupListener
 	private PrivilegeService privilegeService;
 	
 	
+	static {
+		PropertyConfigurator.configure(LOGFILE_LOCATION);
+	}
+	
+	
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent arg0) {
-		// TODO: logging
-		System.out.println("WebAppStartupListener.onApplicationEvent: start");
+		LOGGER.debug("WebAppStartupListener.onApplicationEvent: start");
+		
 		Role role;
+		LOGGER.debug("role -- System Administrator check..");
 		if ((role = roleService.selectByName("System Administrator")) == null) {
 			role = new Role("System Administrator");
 			roleService.save(role);
-			// TODO: logging
-			System.out.println("WebAppStartupListener.onApplicationEvent: role " + role.getName() + " was created..");
+			
+			LOGGER.debug("role -- " + role.getName() + " was created..");
 		}
 		
 		User sudoer;
+		LOGGER.debug("user -- sudoer check..");
 		if ((sudoer = userService.selectByName("sudoer")) == null) {
 			sudoer = new User("sudoer", 
 					   		  "sudoer7", 
@@ -54,15 +69,16 @@ public class WebAppStartupListener
 			
 			role.addUser(sudoer);
 			roleService.save(role);
-			// TODO: loggging
-			System.out.println("WebAppStartupListener.onApplicationEvent: user " + sudoer.getUsername() + " was created..");
+			
+			LOGGER.debug("user -- " + sudoer.getUsername() + " was created as a " + role.getName());
 		}
 		
+		LOGGER.debug("role -- Reader check..");
 		if ((role = roleService.selectByName("Reader")) == null) {
 			role = new Role("Reader");
 			roleService.save(role);
-			// TODO: logging
-			System.out.println("WebAppStartupListener.onApplicationEvent: role " + role.getName() + " was created..");
+			
+			LOGGER.debug("role -- " + role.getName() + " was created..");
 		}
 	}
 }
